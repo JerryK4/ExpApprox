629316b0ca4fd0b07b68ee38dfcc11679f4eebcb
Đoàn Đức Mạnh <163757972+JerryK4@users.noreply.github.com>
2025-12-29 16:45:48 +0700

    Update top.vhd

--- ExpApprox/top.vhd
+++ ExpApprox/top.vhd
@@ -6,18 +6,12 @@ entity cordic_system_top is
     port (
         clk     : in  std_logic;
         rst     : in  std_logic;
-        start   : in  std_logic; -- Tín hi?u kích ho?t b?t ??u quy trình
-        done    : out std_logic  -- Báo hi?u hoàn thành toàn b? m?ng d? li?u
+        start   : in  std_logic; 
+        done    : out std_logic  
     );
 end entity;
 
 architecture structural of cordic_system_top is
-
-    --------------------------------------------------------------------
-    -- COMPONENT DECLARATIONS
-    --------------------------------------------------------------------
-    
-    -- B? vi x? lı (G?m Control Unit và Datapath)
     component microprocessor is
         port (
             clk         : in  std_logic;
@@ -32,7 +26,6 @@ architecture structural of cordic_system_top is
         );
     end component;
 
-    -- B? nh? Unified Memory (64 ô nh? x 16-bit)
     component memory is
         port (
             clk      : in  std_logic;
@@ -45,40 +38,33 @@ architecture structural of cordic_system_top is
         );
     end component;
 
-    --------------------------------------------------------------------
-    -- INTERNAL SIGNALS (Bus k?t n?i)
-    --------------------------------------------------------------------
     signal s_mem_addr     : std_logic_vector(5 downto 0);
-    signal s_data_p_to_m  : std_logic_vector(15 downto 0); -- Processor ghi vào Memory
-    signal s_data_m_to_p  : std_logic_vector(15 downto 0); -- Memory g?i cho Processor
+    signal s_data_p_to_m  : std_logic_vector(15 downto 0); 
+    signal s_data_m_to_p  : std_logic_vector(15 downto 0); 
     signal s_mre, s_mwe   : std_logic;
 
 begin
 
-    --------------------------------------------------------------------
-    -- MICROPROCESSOR INSTANTIATION
-    --------------------------------------------------------------------
+
     U_PROCESSOR: microprocessor
         port map (
             clk          => clk,
             rst          => rst,
             start        => start,
             
-            -- K?t n?i bus d? li?u và ??a ch?
-            mem_data_in  => s_data_m_to_p, -- ??c t? RAM
-            mem_addr     => s_mem_addr,    -- G?i ??a ch? t?i RAM
-            mem_data_out => s_data_p_to_m, -- K?t qu? exp_out g?i t?i RAM
+            -- K?t n?i bus d? li?u vÃ  ??a ch?
+            mem_data_in  => s_data_m_to_p, 
+            mem_addr     => s_mem_addr,    
+            mem_data_out => s_data_p_to_m, 
             
-            -- K?t n?i tín hi?u ?i?u khi?n RAM
+            -- K?t n?i tÃ­n hi?u ?i?u khi?n RAM
             Mre          => s_mre,
             Mwe          => s_mwe,
             
             done         => done
         );
 
-    --------------------------------------------------------------------
-    -- MEMORY INSTANTIATION
-    --------------------------------------------------------------------
+
     U_MEMORY: memory
         port map (
             clk      => clk,
@@ -92,3 +78,4 @@ begin
         );
 
 end structural;
+
